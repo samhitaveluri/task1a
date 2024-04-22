6b7d39e0a8526ce72088415e9fd1b5fe23ffb57e
samhitaveluri <121224422+samhitaveluri@users.noreply.github.com>
2024-04-22 21:50:49 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Home = () => {
+  return ( 
+          <Link to="/desktopseventyfive" style={{ color: "#87CEFA", textDecoration: "none" }}>
+            DesktopSeventyFive
+          </Link>
+    
+  );
+};
+export default Home;
